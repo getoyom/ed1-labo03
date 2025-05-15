c99c481dbf1a85ae347ca5f7601e4daa64691563
getoyom <getoyom@correo.url.edu.gt>
2025-05-14 23:54:54 -0600

    cambios y ejercicio 2

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -7,8 +7,9 @@ import java.util.Queue;
 
 public class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
+        //Hashmap para almacenar la cantidad de apariciones de un número
         HashMap<Integer, Integer> map = new LinkedHashMap<>();
-
+        //Almacenar valores en el HashMap
         for (int num : nums) {
             if(map.containsKey(num)) {
                 map.put(num, map.get(num) + 1);
@@ -16,18 +17,21 @@ public class E01TopKFrequentElements {
                 map.put(num, 1);
             }
         }
+        //Cola de prioridades
         Queue<Integer> heap = new PriorityQueue<>((a,b)->map.get(a) - map.get(b));
+        // Recorre todas las entradas del mapa y agrega las claves al heap.
         for(var entry: map.entrySet()) {
             heap.offer(entry.getKey());
-
+            //Si el tamaño del heap es mayor a k, eliminar el elemento más pequeño
             if(heap.size() > k) {
                 heap.poll();
             }
         }
-
+        //Retornar un resultado de tamaño k
         int[] result = new int[k];
         int index = 0;
         while(!heap.isEmpty()) {
+            //Obtener los dos valores más grandes del heap
             result[index] = heap.poll();
             index++;
         }
--- src/main/java/ed/lab/E02AutocompleteSystem.java
+++ src/main/java/ed/lab/E02AutocompleteSystem.java
@@ -1,13 +1,82 @@
 package ed.lab;
-import java.util.List;
+import java.util.*;
 
 public class E02AutocompleteSystem {
-
+    HashMap<String, Integer> map = new LinkedHashMap<>();
+    Trie trie = new Trie();
+    StringBuilder sb = new StringBuilder();
     public E02AutocompleteSystem(String[] sentences, int[] times) {
-
+        for (int i = 0; i < sentences.length; i++) {
+            map.put(sentences[i], times[i]);
+            trie.insert(sentences[i]);
+        }
     }
+    PriorityQueue<String> pq = new PriorityQueue<>((a,b)->{
+        if(map.get(a)==map.get(b)){
+            return b.compareTo(a);
+        } else{return map.get(a)-map.get(b);}});
 
     public List<String> input(char c) {
-        return null;
+        sb.append(c);
+        if(!trie.startsWith(sb.toString())){
+            return new ArrayList<>();
+        }
+        if(c=='#'){
+            trie.insert(sb.toString());
+            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+            return new ArrayList<>();
+        }
+        for (String key : map.keySet()) {
+            if (key.startsWith(sb.toString())) {
+                pq.offer(key);
+            }
+            if(pq.size() > 3) {
+                pq.poll();
+            }
+        }
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            result.add(pq.poll());
+        }
+        return result;
+    }
+}
+
+//Own Trie implementation
+class Trie {
+    private Node root;
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node current = root;
+        for(char c:word.toCharArray()){
+            if(current.children[c-'a']==null){
+                current.children[c-'a'] = new Node();
+            }
+            current = current.children[c-'a'];
+        }
+        current.isLast=true;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node current = root;
+        for(char c:prefix.toCharArray()){
+            if(current.children[c-'a']==null)
+                return false;
+
+            current=current.children[c-'a'];
+        }
+        return true;
+    }
+
+    private static class Node{
+        public Node[] children;
+        public boolean isLast;
+        public Node(){
+            children = new Node[26];
+            isLast=false;
+        }
     }
 }
